44f891278abb9c2cdff398b2ab7829a04baa56df
Adam Perez <aperez259@mail.valenciacollege.edu>
2016-03-16 22:18:46 -0400

    Changes

--- calculator.py
+++ calculator.py
@@ -1,20 +1,20 @@
 def add( first, second):
-    # TODO:
-    # there's an error in this code, fix it
-    return first plus second
+    return first + second #This will add the first number with the second number
+   
 
 def subtract( first, second):
-    # TODO:
-    # fill in code here that will return the difference between first and second
-
+    
+    return first - second #This will subtract the first number with the second number
+   
 def multiply( first, second):
-    # TODO:
-    # fill in code here that will return the product of first and second
+    
+    return first * second #This will multiply the first number with the second number
+    
 
 def divide( first, second):
-    # TODO:
-    # fill in code here that:
-    #   1. checks the second number to see if it is zero
-    #   2. if the second number is zero, an exception is raised, the exception text must say exactly the following (make sure everything including casing and spaces match)
-    #       I'm sorry, I can't divide by zero
-    #   3. returns the quotient of first and second
+
+    if second == 0: #This will check to see is 0 was entered for the second number
+        raise Exception("I'm sorry, I can't divide by zero") #If the second number is 0, then it will give the Exception
+    else: 
+        return first / second #If the number is not 0, it will calculate the division
+
